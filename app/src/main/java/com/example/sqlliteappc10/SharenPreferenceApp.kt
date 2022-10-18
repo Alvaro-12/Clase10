2da09900b50d8d1a7b70b612c18e6cf9827d924d
Alvaro-12 <lt0895032020@unab.edu.sv>
2022-10-18 10:44:16 -0600

    medias

--- app/src/main/java/com/example/sqlliteappc10/SharenPreferenceApp.kt
+++ app/src/main/java/com/example/sqlliteappc10/SharenPreferenceApp.kt
@@ -1,15 +1,32 @@
 package com.example.sqlliteappc10
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 
 class SharenPreferenceApp : AppCompatActivity() {
 
-    //lateinit var etnNombre
+    lateinit var etnNombre: EditText
+    lateinit var etnApellido :EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sharen_preference_app)
+
+
+
     }
+
+   fun guardar()
+   {
+       var pref = getSharedPreferences("datospersona", Context.MODE_PRIVATE)
+       var editor = pref.edit()
+
+       editor.putString("nombre",etnNombre.text.toString())
+       editor.putString("apellido", etnApellido.toString())
+
+   }
+
+
 }
