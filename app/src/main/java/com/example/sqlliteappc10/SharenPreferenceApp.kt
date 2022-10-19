e86023c22066dc969910c5a4d03a5ee08eba6a05
Alvaro Lopez <lt0895032020@unab.edu.sv>
2022-10-18 21:36:05 -0600

    .

--- app/src/main/java/com/example/sqlliteappc10/SharenPreferenceApp.kt
+++ app/src/main/java/com/example/sqlliteappc10/SharenPreferenceApp.kt
@@ -3,7 +3,9 @@ package com.example.sqlliteappc10
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
 class SharenPreferenceApp : AppCompatActivity() {
 
@@ -14,18 +16,20 @@ class SharenPreferenceApp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sharen_preference_app)
 
-
+        etnNombre = findViewById(R.id.etnNombre)
+        etnApellido = findViewById(R.id.etnApellido)
 
     }
 
-   fun guardar()
+   fun guardar(v:View)
    {
        var pref = getSharedPreferences("datospersona", Context.MODE_PRIVATE)
        var editor = pref.edit()
 
        editor.putString("nombre",etnNombre.text.toString())
        editor.putString("apellido", etnApellido.toString())
-
+       editor.commit()
+       Toast.makeText(this, "se a guardado exitosamente",Toast.LENGTH_LONG).show()
    }
 
 
